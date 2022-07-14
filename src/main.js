8b6448f3ee593e03608edeceaa1264c2f5d9642e
Eugene Kozakov <lointainyyy@gmail.com>
2022-07-14 10:11:48 +0300

    added fontawesome

--- src/main.js
+++ src/main.js
@@ -1,6 +1,11 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { fas } from '@fortawesome/free-solid-svg-icons'
+library.add(fas)
+
 import './assets/scss/main.scss'
 
-createApp(App).mount('#app')
+createApp(App).component('fa', FontAwesomeIcon).mount('#app')
