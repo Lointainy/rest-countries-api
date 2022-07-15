b45a731afb42878f140f29eb20b7a081a5532844
Eugene Kozakov <lointainyyy@gmail.com>
2022-07-15 08:35:51 +0300

    added router link for detailpage

--- src/router.js
+++ src/router.js
@@ -1,7 +1,11 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import MainPage from './Page/MainPage.vue'
+import DetailPage from './Page/DetailPage.vue'
 
 export default createRouter({
   history: createWebHistory(),
-  routes: [{ path: '/home/:countryname?', component: MainPage, alias: '/' }],
+  routes: [
+    { path: '/home', component: MainPage, alias: '/' },
+    { path: '/detailpage/:countryname?', component: DetailPage },
+  ],
 })
