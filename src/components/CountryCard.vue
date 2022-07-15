606f2a613fec8aa380ef9cd722e1728a22fe7e09
Eugene Kozakov <lointainyyy@gmail.com>
2022-07-15 08:14:48 +0300

    fix style

--- src/components/CountryCard.vue
+++ src/components/CountryCard.vue
@@ -1,16 +1,16 @@
 <template>
   <div v-for="country in countryList" :key="country" class="country-list__item">
-    <img class="country-list__item-img" :src="country.flags.png" :alt="`${country.name.common} img`" />
-    {{}}
-    <div class="country-list__item-field">
-      <router-link :to="`/detailpage/${country.name.common}`">{{ country.name.common }}</router-link>
-      <div class="field-title">{{ country.name.common }}</div>
-      <div class="field-subtitle"><span class="bold">Population</span>{{ country.population.toLocaleString() }}</div>
-      <div class="field-subtitle"><span class="bold">Region:</span>{{ country.region }}</div>
-      <div v-if="country.capital" class="field-subtitle">
-        <span class="bold">Capital:</span>{{ country.capital.join(', ') }}
+    <router-link :to="`/detailpage/${country.name.common}`">
+      <img class="country-list__item-img" :src="country.flags.png" :alt="`${country.name.common} img`" />
+      <div class="country-list__item-field">
+        <div class="field-title">{{ country.name.common }}</div>
+        <div class="field-subtitle"><span class="bold">Population</span>{{ country.population.toLocaleString() }}</div>
+        <div class="field-subtitle"><span class="bold">Region:</span>{{ country.region }}</div>
+        <div v-if="country.capital" class="field-subtitle">
+          <span class="bold">Capital:</span>{{ country.capital.join(', ') }}
+        </div>
       </div>
-    </div>
+    </router-link>
   </div>
 </template>
 
@@ -23,7 +23,7 @@ const countryList = inject('countryList')
 .country-list__item {
   flex-grow: 1;
   max-width: 16.5rem;
-  height: 21rem;
+  height: 22rem;
   background-color: var(--nav-bar-color);
   border-radius: $border-radius;
   box-shadow: var(--box-shadow-color);
@@ -62,11 +62,6 @@ const countryList = inject('countryList')
       font-weight: 300;
       font-size: 0.875rem;
       line-height: 1rem;
-
-      .bold {
-        margin-right: 0.5rem;
-        font-weight: 600;
-      }
     }
   }
 }
