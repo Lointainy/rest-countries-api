1acb1512f99d0a4a2595ca7bde1192fc911e47b2
Eugene Kozakov <lointainyyy@gmail.com>
2022-07-20 22:04:02 +0300

    added loader logic

--- src/store/countries.js
+++ src/store/countries.js
@@ -4,6 +4,7 @@ export const useStore = defineStore('countries', {
   state: () => ({
     countries: [],
     country: null,
+    isLoading: false,
   }),
 
   getters: {
@@ -14,10 +15,20 @@ export const useStore = defineStore('countries', {
 
   actions: {
     getCountriesFromApi() {
+      this.isLoading = true
       return fetch('https://restcountries.com/v3.1/all')
-        .then((res) => res.json())
-        .then((data) => (this.countries = data))
-        .catch((err) => console.log(err.message))
+        .then((res) => {
+          console.log(this.isLoading)
+          return res.json()
+        })
+        .then((data) => {
+          this.countries = data
+          this.isLoading = false
+          console.log(this.isLoading)
+        })
+        .catch((err) => {
+          console.log(err.message)
+        })
     },
 
     getCountryFromApi(name) {
